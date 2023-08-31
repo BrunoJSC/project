7db7ff1040c43c43b9775e72c42db37f2c1fb880
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-08-30 22:58:14 -0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   resolve: {
     alias: {
-      react: "https://cdn.skypack.dev/react@17",
-      "react-dom": "https://cdn.skypack.dev/react-dom@17",
+      react: "https://cdn.skypack.dev/react@18",
+      "react-dom": "https://cdn.skypack.dev/react-dom@18",
     },
   },
   plugins: [react()],
